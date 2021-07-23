520ec97543c9f666fa4c42d7c238c648426dd091
webkoth <webkoth@gmail.com>
2021-07-23 17:34:29 +0300

    Added slug method

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,44 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title', 'description', 'content', 'slug', 'category_id'
+    ];
+
+    public function tags(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function setSlugAttribute($value) {
+
+        if (static::whereSlug($slug = Str::slug($value))->exists()) {
+            $slug = $this->incrementSlug($slug);
+        }
+        $this->attributes['slug'] = $slug;
+    }
+
+    public function incrementSlug($slug) {
+
+        $original = $slug;
+        $count = 1;
+
+        while (static::whereSlug($slug)->exists()) {
+            $slug = "{$original}-" . $count++;
+        }
+        return $slug;
+    }
 }
