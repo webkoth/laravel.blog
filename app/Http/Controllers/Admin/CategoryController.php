ca104e35b79e811f960385c0c3194469b5e58ec3
webkoth <webkoth@gmail.com>
2021-07-23 17:36:41 +0300

    Creating CategoryController

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class CategoryController extends Controller
+{
+
+    public function index(): Factory|View|Application
+    {
+        $categories = Category::all();
+
+
+        return view('admin.categories.index', compact('categories'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Application|Factory|View
+     */
+    public function create(): View|Factory|Application
+    {
+        return view('admin.categories.create');
+    }
+
+
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'title' => 'required'
+        ]);
+
+        Category::create([
+            'title' => $request->title,
+            'slug' => $request->title
+        ]);
+
+        return redirect()->route('categories.index')->with('success', __('Категория добавлена'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param Category $category
+     * @return Application|Factory|View
+     */
+    public function edit(Category $category): View|Factory|Application
+    {
+        return view('admin.categories.edit', compact('category'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param Category $category
+     * @return RedirectResponse
+     */
+    public function update(Request $request, Category $category): RedirectResponse
+    {
+
+
+        $request->validate([
+            'title' => 'required',
+            'slug' => 'required|unique:categories'
+        ]);
+
+        $category->update($request->all());
+
+        return redirect()->route('categories.index')->with('success', 'Изменения сохранены');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return RedirectResponse
+     */
+    public function destroy(Category $category): RedirectResponse
+    {
+        $category->delete();
+
+        return back()->with('success', 'Категория удалена');
+    }
+}
