dbacd7a529d7abf7b621310467c6dbf3abc98dd0
webkoth <webkoth@gmail.com>
2021-07-23 17:32:49 +0300

    Added method for creating unique slug

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,41 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'slug'
+    ];
+
+
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
+
+
+    public function setSlugAttribute($value) {
+
+        if (static::whereSlug($slug = Str::slug($value))->exists()) {
+            $slug = $this->incrementSlug($slug);
+        }
+        $this->attributes['slug'] = $slug;
+    }
+
+    public function incrementSlug($slug) {
+
+        $original = $slug;
+        $count = 1;
+
+        while (static::whereSlug($slug)->exists()) {
+            $slug = "{$original}-" . $count++;
+        }
+        return $slug;
+    }
 }
