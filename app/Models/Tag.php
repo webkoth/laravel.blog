e3ca235638a7b90f7b5e9fd4f853a1fcaa7067c7
webkoth <webkoth@gmail.com>
2021-07-23 17:35:49 +0300

    Added slug method and relations

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,8 +4,41 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Str;
 
 class Tag extends Model
 {
     use HasFactory;
+
+
+    public function posts(): BelongsToMany
+    {
+        return $this->belongsToMany(Post::class);
+    }
+
+
+    public function setSlugAttribute($value) {
+
+        if (static::whereSlug($slug = Str::slug($value))->exists()) {
+            $slug = $this->incrementSlug($slug);
+        }
+        $this->attributes['slug'] = $slug;
+    }
+
+    /**
+     * Increment number to slug for unique value
+     * @param $slug
+     * @return mixed|string
+     */
+    public function incrementSlug($slug) {
+
+        $original = $slug;
+        $count = 1;
+
+        while (static::whereSlug($slug)->exists()) {
+            $slug = "{$original}-" . $count++;
+        }
+        return $slug;
+    }
 }
