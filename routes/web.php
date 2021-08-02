25a7e8fd6e6e610e73e8b1a4ca277170581dede3
webkoth <webkoth@gmail.com>
2021-08-02 10:42:33 +0300

    Added tags and posts route

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,6 @@ Route::get('/', function () {
 Route::prefix('admin')->namespace('Admin')->group(function () {
     Route::get('/', 'MainController@index')->name('admin.index');
     Route::resource('/categories', 'CategoryController')->except('show');
+    Route::resource('/tags', 'TagController')->except('show');
+    Route::resource('/posts', 'PostController');
 });
