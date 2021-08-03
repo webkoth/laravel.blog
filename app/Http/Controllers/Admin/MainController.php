4d953541d55d73e6df331edc902e414ef464022e
webkoth <webkoth@gmail.com>
2021-08-03 18:38:04 +0300

    Added index method to dashboard

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    public function index(): string
+    public function index(): Factory|View|Application
     {
-        return view('admin.index');
+        $posts = Post::all();
+        $tags = Tag::all();
+        $categories = Category::all();
+
+        return view('admin.index', compact('posts', 'tags', 'categories'));
     }
 }
