69d5730743684df34afa33797ed9c8cd060bc42b
webkoth <webkoth@gmail.com>
2021-08-03 16:11:53 +0300

    Creating PostController

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -3,83 +3,144 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Application|Factory|View
      */
-    public function index()
+    public function index(): Application|Factory|View
     {
-        //
+        $posts = Post::all();
+
+        return view('admin.posts.index', compact('posts'));
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Application|Factory|View
      */
-    public function create()
+    public function create(): Application|Factory|View
     {
-        //
+        $categories = Category::pluck('title', 'id');
+
+        $tags = Tag::pluck('title', 'id');
+
+        return view('admin.posts.create', [
+            'categories' => $categories,
+            'tags' => $tags
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return RedirectResponse
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        //
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'content' => 'required',
+            'category_id' => 'required|int',
+            'thumbnail' => 'nullable|image'
+        ]);
+
+        $data = $request->all();
+
+        $data['thumbnail'] = Post::uploadImage($request);
+
+        $post = Post::make($data);
+        $post->slug = $request->title;
+        $post->save();
+        $post->tags()->sync($request->input('tags'));
+
+        return redirect()->route('posts.index')->with('success', 'Статья добавлена');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Post $post
+     * @return Application|Factory|View
      */
-    public function show($id)
+    public function show(Post $post): View|Factory|Application
     {
-        //
+        return view('admin.posts.show', compact('post'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Post $post
+     * @return Application|Factory|View
      */
-    public function edit($id)
+    public function edit(Post $post): View|Factory|Application
     {
-        //
+        $categories = Category::pluck('title', 'id');
+
+        $tags = Tag::pluck('title', 'id');
+
+
+        return view('admin.posts.edit', compact('post', 'categories', 'tags'));
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param Post $post
+     * @return RedirectResponse
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Post $post): RedirectResponse
     {
-        //
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'content' => 'required',
+            'category_id' => 'required|int',
+            'thumbnail' => 'nullable|image'
+        ]);
+
+        $data = $request->all();
+
+        $data['thumbnail'] = Post::uploadImage($request, $post->thumbnail);
+
+        $post->update($data);
+
+        $post->tags()->sync($request->input('tags'));
+
+        return redirect()->route('posts.index')->with('success', 'Изменения сохранены');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Post $post
+     * @return RedirectResponse
      */
-    public function destroy($id)
+    public function destroy(Post $post): RedirectResponse
     {
-        //
+        $post->tags()->sync([]);
+
+        Storage::delete($post->thumbnail);
+
+        $post->delete();
+
+        return redirect()->route('posts.index')->with('success', 'Пост удален');
     }
 }
